4bdae7646b30947b8cf55d8cb79718dd12b28f2d
Glenn Streetman <glennstreetman@gmail.com>
2020-12-21 17:25:24 -0800

    basic template for widget body created

---
+++ src/templates/widgetBodyTemplate.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import StockSearchPane, {searchPaneProps} from "../../../components/stockSearchPane.js";
+import {finnHub} from "../../../appFunctions/throttleQueue.js";
+
+export default class widgetName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+      this.baseState = {mounted: true}
+      this.renderSearchPane = this.renderSearchPane.bind(this);
+      this.renderStockData = this.renderStockData.bind(this);
+      this.getStockData = this.getStockData.bind(this);
+    }
+
+    componentDidMount(){
+      //get starting data?
+    }
+
+    componentDidUpdate(prevProps, prevState){
+      //pass
+    }
+
+    componentWillUnmount(){
+      this.baseState.mounted = false
+    }
+
+    renderSearchPane(){
+        //add search pane rendering logic here. Additional filters need to be added below.
+    }
+
+    renderStockData(){
+        //add widget data here
+    } 
+
+    getStockData(stock){
+      const p = this.props
+      const that = this
+      const queryString = `------------apiEndPointQueryString--------------`
+      finnHub(p.throttle, queryString)
+      .then((data) => {
+        if (that.baseState.mounted === true) {
+          //update state
+          console.log(data)
+        }
+      })
+      .catch(error => {
+        console.log(error.message)
+      });
+    }
+
+    render() {
+        return (
+            <>
+            {this.props.showEditPane === 1 && (
+              <>
+              {React.createElement(StockSearchPane, searchPaneProps(this))}
+              {this.renderSearchPane()}
+              </>
+            )}
+            {Object.keys(this.props.trackedStocks).length > 0 && 
+            this.props.showEditPane === 0  ? this.renderStockData() : <></>}       
+          </>
+        )
+    }
+  }
+
+
+export function newWidgetNameProps(that, key = "newWidgetNameProps") {
+    let propList = {
+      apiKey: that.props.apiKey,
+      showPane: that.showPane,
+      trackedStocks: that.props.widgetList[key]["trackedStocks"],
+      filters: that.props.widgetList[key]["filters"],
+      updateWidgetFilters: that.props.updateWidgetFilters,
+      updateGlobalStockList: that.props.updateGlobalStockList,
+      updateWidgetStockList: that.props.updateWidgetStockList,
+      widgetKey: key,
+      throttle: that.props.throttle,
+      exchangeList: that.props.exchangeList,
+      defaultExchange: that.props.defaultExchange,
+      updateDefaultExchange: that.props.updateDefaultExchange,
+    };
+    return propList;
+  }
+
+
+  // fetch('https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=bsuu7qv48v6qu589jlj0')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
