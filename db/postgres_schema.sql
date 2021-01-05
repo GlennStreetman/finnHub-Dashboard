875fc0d15828ac3f0454dca53ea4426c078d147a
Glenn Streetman <glennstreetman@gmail.com>
2021-01-05 11:55:02 -0800

    updating schema definition

--- db/postgres_schema.sql
+++ db/postgres_schema.sql
@@ -1,38 +1,40 @@
-CREATE TABLE public.dashboard
+//serial for auto increment
+
+CREATE TABLE dashboard
 (
-    id integer NOT NULL DEFAULT nextval('dashboard_id_seq'::regclass),
+    id serial NOT NULL,
     userid integer,
-    dashboardname text COLLATE pg_catalog."default",
-    globalstocklist text COLLATE pg_catalog."default",
-    widgetlist text COLLATE pg_catalog."default",
+    dashboardname text,
+    globalstocklist text,
+    widgetlist text,
     CONSTRAINT dashboard_pkey PRIMARY KEY (id),
     CONSTRAINT dashboardid UNIQUE (userid, dashboardname)
 ),
 
-CREATE TABLE public.menusetup
+CREATE TABLE menusetup
 (
-    id integer NOT NULL DEFAULT nextval('menusetup_id_seq'::regclass),
+    id serial NOT NULL,
     userid integer,
-    menulist text COLLATE pg_catalog."default",
-    defaultmenu text COLLATE pg_catalog."default",
+    menulist text,
+    defaultmenu text,
     CONSTRAINT menusetup_pkey PRIMARY KEY (id),
     CONSTRAINT oneperuser UNIQUE (userid)
 ),
 
 CREATE TABLE public.users
 (
-    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    loginname text COLLATE pg_catalog."default",
-    email text COLLATE pg_catalog."default",
-    password text COLLATE pg_catalog."default",
-    secretquestion text COLLATE pg_catalog."default",
-    secretanswer text COLLATE pg_catalog."default",
-    apikey text COLLATE pg_catalog."default",
-    webhook text COLLATE pg_catalog."default",
-    confirmemail text COLLATE pg_catalog."default",
-    resetpassword text COLLATE pg_catalog."default",
-    exchangelist text COLLATE pg_catalog."default",
-    defaultexchange text COLLATE pg_catalog."default",
+    id serial NOT NULL,
+    loginname text,
+    email text,
+    password text,
+    secretquestion text,
+    secretanswer text,
+    apikey text,
+    webhook text,
+    confirmemail text,
+    resetpassword text,
+    exchangelist text,
+    defaultexchange text,
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT users_email_key UNIQUE (email),
     CONSTRAINT users_loginname_key UNIQUE (loginname)
