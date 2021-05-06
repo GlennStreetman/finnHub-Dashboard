a34ae76f7fd06b6af49c48b2f6ff86c9152308b9
Glenn Streetman <glennstreetman@gmail.com>
2021-05-05 17:32:19 -0700

    minimal functionality added for time series data.

--- src/server/routes/excelTemplates/runTemplate.js
+++ src/server/routes/excelTemplates/runTemplate.js
@@ -21,12 +21,10 @@ const getMongoData = (reqObj) => {
     
     return new Promise((resolve) => {
         const getAPIData = `http://localhost:5000/qGraphQL?query=${reqObj.q}`
-        // console.log(getAPIData)
         fetch(getAPIData)
         .then((r)=>r.json())
         .then(data=>{
             for (const x in data){
-                // console.log(data[x])
                 reqObj.data = data[x]
                 resolve(reqObj)
             }})
@@ -36,15 +34,25 @@ const getMongoData = (reqObj) => {
 function getDataSlice(dataObj, queryString){
     //iterates through all dataObj keys to finds all matching slices 
     //return object {security(s): value(s)}
-    // console.log('queryString', queryString)
+    
     const returnObj = {}
     const queryStringList = queryString.split('.')
     const keys = dataObj.keys
     for (const s of keys){
-        const val = dataObj?.[queryStringList[0]]?.[s]?.[queryStringList[1]]
-        returnObj[s] = val
+        if (typeof dataObj?.[queryStringList[0]]?.[s]?.[queryStringList[1]] !== 'undefined') { 
+            //data point data
+            const val = dataObj?.[queryStringList[0]]?.[s]?.[queryStringList[1]]
+            returnObj[s] = val
+        } else {
+            //time series data
+            const timeSeriesSlice = dataObj?.[queryStringList[0]]?.[s]
+            const val = {}
+            for (const t in timeSeriesSlice) {
+                val[t] = timeSeriesSlice[t][queryStringList[1]]
+            }
+            returnObj[s] = val
+        }
     }
-
     return returnObj
 }
 
@@ -67,7 +75,7 @@ router.get('/runTemplate', async (req, res) => {
     
     //read query worksheet and retrieve data from graphQL
     const queryObj = {}
-    const promiseList = []
+    const promiseList = [] //all data required to update spreadsheet.
 
     if (fs.existsSync(workBookPath)) { //if template exists
         let workbook = xlsx.readFile(workBookPath);
@@ -102,16 +110,15 @@ router.get('/runTemplate', async (req, res) => {
         })
         //make temp directory for user if it doesnt already exist.
         if (!fs.existsSync(tempPath)) {
-            // console.log(tempPath)
             fs.mkdir(tempPath, (err) => {
                 if (err) {
                     return console.error(err);
                 }
-                // console.log('Directory created successfully!');
             })
         }
-        // console.log(promiseData)
+
         //read template, create data object, create temp file to be written to.
+        //update key columns and writecolumn count.
         const templateData = {} 
         // fs.copyFileSync(workBookPath, tempFile)
         let wb = new Excel.Workbook()
@@ -122,35 +129,34 @@ router.get('/runTemplate', async (req, res) => {
                 worksheet.eachRow((row, rowNumber)=>{
                     const thisRow = {
                         data: {},
-                        writeColumns: 0, //if zero skip in later steps. Used for inserting new rows if greater than 1.
+                        writeRows: 0, //if zero skip in later steps. Used for inserting new rows if greater than 1.
                         keyColumns: {}, //list of columns where key needs to be updated.
                     }
                     for (const x in row.values) {
-                        // console.log(typeof row?.values?.[x])
-                        if (typeof row?.values?.[x] === 'string' && row?.values?.[x]?.slice(0,2) === '&=') {
+                        if (typeof row?.values?.[x] === 'string' && row?.values?.[x]?.slice(0,2) === '&=') { //if template formula deted.
                             const searchStringRaw = row.values[x]
                             const searchString = searchStringRaw.slice(2, searchStringRaw.length)
-                            if (searchString !== 'keys.keys') {
-                                const dataObj = getDataSlice(promiseData, searchString)
-                                if (Object.keys(dataObj).length > thisRow.writeColumns) {thisRow.writeColumns = Object.keys(dataObj).length}
+                            if (searchString !== 'keys.keys') { //if data column
+                                const dataObj = getDataSlice(promiseData, searchString) //could {key: string} pairs OR {key: OBJECT} pairs
+                                for (const s in dataObj) {
+                                    if (typeof dataObj[s] === 'object'){ //count number of rows if time series data in dataset.
+                                        thisRow.writeRows = thisRow.writeRows +  Object.keys(dataObj[s]).length
+                                    } 
+                                }
+                                if (Object.keys(dataObj).length > thisRow.writeRows) {thisRow.writeRows = Object.keys(dataObj).length} 
                                 thisRow.data[x] = dataObj
-                            } else {
+                            } else { //if key column
                                 thisRow.data[x] = {...promiseData.keys}
-                                // thisRow.data[x] = {}
-                                // promiseData.keys.forEach((i)=>{thisRow.data[x][i] = i})
                                 thisRow.keyColumns[x] = x
-                                // console.log('Key Column ', thisRow.keyColumns)
                             }
                         }
                     }
-                    // console.log('Row ' + rowNumber + ' : ', thisRow );
                     templateData[worksheet.name][rowNumber] = thisRow
-                    // console.log(templateData[worksheet.name][rowNumber]) 
                 })
             }
         })
-        // console.log(templateData)
 
+        // console.log(templateData['Sheet2']['2']['data']['3'] //['US-WMT'])
         const w = new Excel.Workbook()
         w.xlsx.readFile(workBookPath)
         .then(()=>{ //write templatedata to temporary worksheet copied from source template.
@@ -160,40 +166,78 @@ router.get('/runTemplate', async (req, res) => {
                 const addedRows = []
                 if (s !== 'keys') { 
                     const templateWorksheet = templateData[s]
-                    for (const row in templateWorksheet) { // for each row in worksheet
+                    for (const row in templateWorksheet) { // for each TEMPLATE row in worksheet
                         const data = templateWorksheet[row].data //list of rows to be updated. 
-                        const writeColumns =  templateWorksheet[row].writeColumns //used to create range we need to work through.
+                        const writeRows =  templateWorksheet[row].writeRows //used to create range of rows we need to  update.
                         const keyColumns =  templateWorksheet[row].keyColumns //key columns
                         let currentKey = '' //the current security key
-                        // console.log('row:', row, data, writeColumns, keyColumns)
-                        for (let step = 1; step <= writeColumns; step++) {
+                        let currKeyColumn = 0 //ref to the source of current security key
+                        for (let step = 1; step <= writeRows; step++) { //iterate over new rows that data will populate into
+                            let timeSeries = 0 //switch to 1 if time series data found
+                            let startRow = 0 //saves the start line for time series data
+                            let dataRowCount = 0 //number of rows to enter data for time series
                             for (const updateCell in data) {
-                                if (keyColumns[updateCell]) { 
-                                    currentKey = data[updateCell][step-1]
-                                    ws.getRow(parseInt(row) + rowIterator).getCell(parseInt(updateCell)).value = data[updateCell][step-1]
-                                } else {
+                                if (keyColumns[updateCell]) {  //update if key column
+                                    currentKey = data?.[updateCell]?.[step-1]
+                                    currKeyColumn = updateCell
+                                    if (data[updateCell][step-1] && typeof data[updateCell][step-1] === 'string') {
+                                        ws.getRow(parseInt(row) + rowIterator).getCell(parseInt(updateCell)).value = data[updateCell][step-1]
+                                    }
+                                } else if (typeof data[updateCell][currentKey] !== 'object') { //update if data point
+                                    // console.log('here', data[updateCell][currentKey])
                                     ws.getRow(parseInt(row) + rowIterator).getCell(parseInt(updateCell)).value = data[updateCell][currentKey]
+                                } else { //update if time series
+                                    console.log('currentKey', currentKey)
+                                    const dataGroup = data[updateCell][currentKey]
+                                    // if (dataGroup !== undefined) console.log('---------dataGroup-------------', JSON.stringify(dataGroup, null, 2))
+                                    if (timeSeries === 0) { //only run once.
+                                        
+                                        timeSeries = 1;
+                                        startRow = rowIterator
+                                        dataRowCount = Object.keys(dataGroup).length
+                                        console.log('creating new rows:', dataRowCount)
+                                        for (let i = 1; i <= dataRowCount; i++) {
+                                            let newRow = parseInt(row) + rowIterator + i 
+                                            console.log('newRow', newRow)
+                                            ws.insertRow(newRow).commit()
+                                            addedRows.push(newRow)
+                                            
+                                        }
+                                        rowIterator = rowIterator + dataRowCount 
+                                    }
+                                    
+                                    for (let d = 0; d < dataRowCount;  d++){ //enter data for column
+                                        const key = Object.keys(dataGroup)[d]
+                                        ws.getRow(parseInt(startRow) + d + 2).getCell(parseInt(updateCell)).value = dataGroup[key]
+                                        ws.getRow(parseInt(startRow) + d + 2).getCell(parseInt(currKeyColumn)).value = currentKey
+                                        ws.getRow(parseInt(startRow) + d + 2).commit()
+                                    }
+
                                 }
                             }
-                            ws.getRow(row + rowIterator).commit()
-                            if (step !== writeColumns) {
-                                let newRow = parseInt(row) + rowIterator + 1
-                                ws.insertRow(newRow).commit()
-                                addedRows.push(newRow)
+                            if (timeSeries === 0) {
+                                ws.getRow(row + rowIterator).commit()
+                                if (step !== writeRows) {
+                                    let newRow = parseInt(row) + rowIterator + 1
+                                    ws.insertRow(newRow).commit()
+                                    addedRows.push(newRow)
+                                }
+                                rowIterator = rowIterator + 1
                             }
-                            rowIterator = rowIterator + 1
                         }
                     }
                 }
-                console.log(addedRows) //next update all formula cells. Offset their row references be number of rows added before or inside of formula range.
+                //next update all formula cells. Offset their row references by number of rows added before or inside of formula range.
                 ws.eachRow({includeEmpty: false},(row, rowNumber)=>{
+                    // console.log('getRow', rowNumber)
                     row.eachCell({ includeEmpty: false },(cell, colNumber)=>{
-                        if (typeof cell.value === 'object') {
+                        if (typeof cell.value === 'object') { //if excel formula
+                            console.log(cell.value)
                             let updateString = cell.value.formula
                             let updateList = []
-                            console.log('1:', updateString, typeof updateString)
+                            // console.log('1:', updateString, typeof updateString)
                             let re = new RegExp(/(?:^|[^0-9])([A-Z](?:100|[0-9][0-9]?))(?=$|[^0-9A-Z])/g)
-                            let allMatches = [...cell.value.formula.matchAll(re)]
+                            let allMatches = cell.value.formula.matchAll(re) ? [...cell.value.formula.matchAll(re)] : []
                             for (const m in allMatches) {
                                 let matchRow = parseInt(allMatches[m][1].replace(new RegExp(/\D/g), ''))
                                 let matchColumn = allMatches[m][1].replace(new RegExp(/[0-9]/g), '')
@@ -201,14 +245,14 @@ router.get('/runTemplate', async (req, res) => {
                                     if (matchRow >= parseInt(addedRows[r])) matchRow = matchRow + 1
                                 }
                                 const updateRef = matchColumn + matchRow
-                                console.log('2:', allMatches[m][1], updateRef)
+                                // console.log('2:', allMatches[m][1], updateRef)
                                 updateList.unshift([allMatches[m][1], updateRef]) //update row values in reverse order so you dont double update starting row.
                             }
                             for (const u in updateList) {
-                                console.log('3:',updateString.indexOf(updateList[u][0]), updateList[u][1])
+                                // console.log('3:',updateString.indexOf(updateList[u][0]), updateList[u][1])
                                 updateString = updateString.replace(updateList[u][0], updateList[u][1])
                             }
-                            console.log('4:', updateString)
+                            // console.log('4:', updateString)
                             const newValue = cell.value
                             newValue.formula = updateString
                             ws.getRow(rowNumber).getCell(colNumber).value = newValue
@@ -217,24 +261,10 @@ router.get('/runTemplate', async (req, res) => {
                     ws.getRow(row).commit()
                 })
             }
-            
-
             w.xlsx.writeFile(tempFile)
         })
 
     }
 })
 
-
 export default router
-
-
-// A34A1
-// 5A1
-// 2.344A1
-// A1001
-// A1
-// A10
-// A100
-// SUM(A1:A2)
-// SUM(A1:A2, 5A1, A3)
