a02968b695e789156d42fed856ec833d8dba49db
Glenn Streetman <glennstreetman@gmail.com>
2021-05-26 16:46:29 -0700

    fixed route to work with updated mongo schema

--- src/server/routes/mongoDB/deleteMongoRecords.js
+++ src/server/routes/mongoDB/deleteMongoRecords.js
@@ -6,17 +6,19 @@ const router =  express.Router();
 //Recieves widget key as a paramater. Deletes corresponding records. req.query['user']
 router.get('/deleteFinnDashData', async (req, res) => {
     if (req.session.login === true) {
-        // console.log("-----running delete!-------")
+        console.log("-----running delete!-------", req.query['widgetID'])
         try {
             const client = getDB()
             const database = client.db('finnDash');
             const dataSet = database.collection('dataSet');
-            const widgetSearch = RegExp(`^${req.query['widgetID']}`, 'i')
+            // const widgetSearch = RegExp(`^${req.query['widgetID']}`, 'i')
+            const widgetID = req.query['widgetID']
             const deleteList = await dataSet.deleteMany({ //delete stale records for user
                 userID: req.session.uID,
-                key: {$regex: widgetSearch},
+                widget: widgetID
+                // key: {$regex: widgetSearch},
             })
-            console.log('deleteList.deletedCount: ', req.session.uID, widgetSearch, deleteList.deletedCount)
+            console.log('deleteList.deletedCount: ', req.session.uID, widgetID, deleteList.deletedCount)
             // console.log("-----delete complete-----")
             res.status(200).json({message: 'Records deleted.'})
 
