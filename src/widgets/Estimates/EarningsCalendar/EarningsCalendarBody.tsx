d843bc725304e95912a952cdc23f763ce58acda8
Glenn Streetman <glennstreetman@gmail.com>
2021-06-25 15:41:12 -0700

    implemented custom hooks

--- src/widgets/Estimates/EarningsCalendar/EarningsCalendarBody.tsx
+++ src/widgets/Estimates/EarningsCalendar/EarningsCalendarBody.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
-import { useState, useEffect, useImperativeHandle, forwardRef, useRef } from "react";
+import { useState, forwardRef, useRef, useMemo } from "react";
 
 import { useAppDispatch, useAppSelector } from '../../../hooks';
-import { rBuildVisableData } from '../../../slices/sliceShowData'
-import { tSearchMongoDB } from '../../../thunks/thunkSearchMongoDB'
+import { convertCamelToProper } from './../../../appFunctions/stringFunctions'
 
 import StockSearchPane, { searchPaneProps } from "../../../components/stockSearchPaneFunc";
+
+import { useDragCopy } from './../../widgetHooks/useDragCopy'
+import { useTargetSecurity } from './../../widgetHooks/useTargetSecurity'
+import { useSearchMongoDb } from './../../widgetHooks/useSearchMongoDB'
+import { useBuildVisableData } from './../../widgetHooks/useBuildVisableData'
+import { useUpdateFocus } from './../../widgetHooks/useUpdateFocus'
+import { useStartingFilters } from './../../widgetHooks/useStartingFilters'
+
 
 const useDispatch = useAppDispatch
 const useSelector = useAppSelector
@@ -36,44 +43,9 @@ interface filters {
     startDate: number,
 }
 
-//add any additional type guard functions here used for live code.
-function isFinnHubData(arg: any): arg is finnHubDataObj { //typeguard
-    if (arg !== undefined && Object.keys(arg).length > 0 && arg.earningsCalendar && arg.earningsCalendar[0] && arg.earningsCalendar[0].date) {
-        // console.log("returning true", arg)
-        return true
-    } else {
-        // console.log("returning false", arg)
-        return false
-    }
-}
-
 function EstimatesEarningsCalendar(p: { [key: string]: any }, ref: any) {
 
     const isInitialMount = useRef(true); //update to false after first render.
-
-    const startingstockData = () => {
-        if (isInitialMount.current === true) {
-            if (p.widgetCopy && p.widgetCopy.widgetID === p.widgetKey) {
-                const stockData = JSON.parse(JSON.stringify(p.widgetCopy.stockData))
-                return (stockData)
-            } else {
-                return ([])
-            }
-        }
-    }
-
-    const startingTargetStock = () => { //REMOVE IF TARGET STOCK NOT NEEDED.
-        if (isInitialMount.current === true) {
-            if (p.widgetCopy && p.widgetCopy.widgetID === p.widgetKey) {
-                const targetStock = p.widgetCopy.targetStock
-                return (targetStock)
-            } else if (p?.config?.targetSecurity) {
-                return (p?.config?.targetSecurity)
-            } else {
-                return ('')
-            }
-        }
-    }
 
     const startingWidgetCoptyRef = () => {
         if (isInitialMount.current === true) {
@@ -99,95 +71,50 @@ function EstimatesEarningsCalendar(p: { [key: string]: any }, ref: any) {
         return endDate
     }
 
+    const startingPagination = () => {
+        if (p.widgetCopy && p.widgetCopy.widgetID === p.widgetKey) {
+            return (p.widgetCopy.pagination)
+        } else { return (0) }
+    }
+
     const [widgetCopy] = useState(startingWidgetCoptyRef())
-    const [stockData, setStockData] = useState(startingstockData());
-    const [targetStock, setTargetStock] = useState(startingTargetStock());
-    const [display, setDisplay] = useState('EPS') //EPS or Revenue
     const [start, setStart] = useState(startingStartDate())
     const [end, setEnd] = useState(startingEndDate())
+    const [pagination, setPagination] = useState(startingPagination())
     const dispatch = useDispatch(); //allows widget to run redux actions.
+    const targetStock = p?.config?.targetSecurity ? p.config.targetSecurity : p?.trackedStocks?.[0] ? p?.trackedStocks?.[0] : ''
 
     const rShowData = useSelector((state) => { //REDUX Data associated with this widget.
         if (state.dataModel !== undefined &&
             state.dataModel.created !== 'false' &&
             state.showData.dataSet[p.widgetKey] !== undefined) {
-            const showData: object = state.showData.dataSet[p.widgetKey][targetStock]
+            const showData: FinnHubAPIDataArray = state.showData.dataSet?.[p.widgetKey]?.[targetStock]?.['earningsCalendar']
             return (showData)
         }
     })
 
-    useImperativeHandle(ref, () => (
-        //used to copy widgets when being dragged. example: if widget body renders time series data into chart, copy chart data.
-        //add additional slices of state to list if they help reduce re-render time.
-        {
-            state: {
-                stockData: stockData,
-                targetStock: targetStock, //REMOVE IF NO TARGET STOCK
-                display: display,
-            },
+    const updateFilterMemo = useMemo(() => {
+        return {
+            startDate: start,
+            endDate: end,
+            Description: 'Date numbers are millisecond offset from now. Used for Unix timestamp calculations.'
         }
-    ))
+    }, [start, end])
 
-    useEffect((key: number = p.widgetKey, trackedStock = p.trackedStocks, keyList: string[] = Object.keys(p.trackedStocks), updateWidgetConfig: Function = p.updateWidgetConfig) => {
-        //Setup default metric source if none selected.
-        if (p.config.targetSecurity === undefined) {
-            const newSource: string = keyList.length > 0 ? trackedStock[keyList[0]].key : ''
-            updateWidgetConfig(key, {
-                targetSecurity: newSource,
-            })
-        }
-    }, [p.updateWidgetConfig, p.widgetKey, p.trackedStocks, p.apiKey, p.config.targetSecurity])
-
-    useEffect(() => {
-        //On mount, use widget copy, else build visable data.
-        //On update, if change in target stock, rebuild visable data.
-        if (isInitialMount.current === true && widgetCopy === p.widgetKey) {
-            isInitialMount.current = false;
-        } else {
-            if (isInitialMount.current === true) { isInitialMount.current = false }
-            const payload: object = {
-                key: p.widgetKey,
-                securityList: [[`${targetStock}`]]
-            }
-            // console.log(payload)
-            dispatch(rBuildVisableData(payload))
-        }
-    }, [targetStock, p.widgetKey, widgetCopy, dispatch])
-
-    useEffect((filters: filters = p.filters, update: Function = p.updateWidgetFilters, key: number = p.widgetKey) => {
-        if (filters['startDate'] === undefined) { //if filters not saved to props
-            const filterUpdate = {
-                startDate: start,
-                endDate: end,
-                Description: 'Date numbers are millisecond offset from now. Used for Unix timestamp calculations.'
-            }
-            update(key, filterUpdate)
-        }
-    }, [p.filters, p.updateWidgetFilters, p.widgetKey, start, end])
-
-    useEffect(() => {
-        //DELETE IF NO TARGET STOCK
-        //if stock not selected default to first stock.
-        if (Object.keys(p.trackedStocks).length > 0 && targetStock === '') {
-            const setDefault = p.trackedStocks[Object.keys(p.trackedStocks)[0]].key
-            setTargetStock(setDefault)
-        }
-    }, [p.trackedStocks, targetStock])
-
-    useEffect(() => { //on update to redux data, update widget stock data, as long as data passes typeguard.
-        if (isFinnHubData(rShowData) === true) { setStockData(rShowData) } else { setStockData([]) }
-    }, [rShowData])
-
-    useEffect(() => { //on change to targetSecurity update widget focus
-        if (p.targetSecurity !== '') {
-            const target = `${p.widgetKey}-${p.targetSecurity}`
-            setTargetStock(p.targetSecurity)
-            dispatch(tSearchMongoDB([target]))
-        }
-    }, [p.targetSecurity, p.widgetKey, dispatch])
+    useDragCopy(ref, { pagination: pagination, })//useImperativeHandle. Saves state on drag. Dragging widget pops widget out of component array causing re-render as new component.
+    useTargetSecurity(p.widgetKey, p.trackedStocks, p.updateWidgetConfig, p?.config?.targetSecurity,) //sets target security for widget on mount and change to security focus from watchlist.
+    useSearchMongoDb(p.config.targetSecurity, p.widgetKey, dispatch) //on change to target security retrieve fresh data from mongoDB
+    useBuildVisableData(p?.config?.targetSecurity, p.widgetKey, widgetCopy, dispatch, isInitialMount) //rebuild visable data on update to target security
+    useUpdateFocus(p.targetSecurity, p.updateWidgetConfig, p.widgetKey) //on update to security focus, from watchlist menu, update target security.
+    useStartingFilters(p.filters['startDate'], updateFilterMemo, p.updateWidgetFilters, p.widgetKey)
 
     function updateStartDate(e) {
         setStart(e.target.value)
+    }
+
+    function changePagination(e) {
+        const newIncrement = pagination + e;
+        if (newIncrement > 0 && newIncrement < Object.keys(rShowData ? rShowData : 1).length) setPagination(newIncrement)
     }
 
     function updateEndDate(e) {
@@ -245,90 +172,58 @@ function EstimatesEarningsCalendar(p: { [key: string]: any }, ref: any) {
     }
 
     function stockTable() {
-        const actual = display === 'EPS' ? 'epsActual' : 'revenueActual'
-        const estimate = display === 'EPS' ? 'epsEstimate' : 'revenueEstimate'
-        let sortedData = stockData['earningsCalendar'] !== undefined ? [...stockData['earningsCalendar']].sort((a, b) => (new Date(a.date) > new Date(b.date) ? 1 : -1)) : []
-        let tableData = sortedData.map((el) => {
-            return <tr key={"row" + el.date}>
-                <td className='rightTE' key={"period" + el.date}> {`${el['year']} Q:${el['quarter']}`} </td>
-                {/* <td className='rightTE' key={"estimate" + el.date}>{Number(el[estimate]).toFixed(2)}</td>             */}
-                <td className='rightTE' key={"estimate" + el.date}>{Number(el[estimate]).toLocaleString(
-                    undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2, })}
-                </td>
-                <td className='rightTE' key={"actual" + el.date}>{Number(el[actual]).toLocaleString(
-                    undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2, })}
-                </td>
-                <td className='rightTE' key={"var" + el.date}>{Number(el[actual] - el[estimate]).toLocaleString(
-                    undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2, })}
-                </td>
-                <td className='rightTE' key={"var2" + el.date}>{Number(
-                    ((el[actual] - el[estimate]) / el[estimate]) * 100
-                ).toLocaleString(
-                    undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2, })}
-                </td>
+        let tableData = rShowData && rShowData?.[pagination] ? Object.entries(rShowData[pagination]).map((el) => {
+            const heading = el[0] ? el[0] : ''
+            const value = el[1] ? el[1] : ''
+            return <tr key={"row" + heading + pagination}>
+                <td className='rightTE' key={"name" + heading}>{convertCamelToProper(el[0])}</td>
+                <td className='rightTE' key={"vale" + value}>{value}</td>
             </tr>
-        })
+        }) : <></>
         return tableData
-    }
-
-    function changeValueSelection(e) {
-        const target = e.target.value;
-        setDisplay(target)
     }
 
     function changeStockSelection(e) { //DELETE IF no target stock
         const target = e.target.value;
-        const key = `${p.widgetKey}-${target}`
-        setTargetStock(target)
         p.updateWidgetConfig(p.widgetKey, {
             targetSecurity: target,
         })
-        dispatch(tSearchMongoDB([key]))
     }
 
     function renderStockData() {
-        let newSymbolList = ["EPS", "Revenue"].map((el) => (
-            <option key={el} value={el}>
-                {el}
-            </option>
-        ));
         let newStockList = Object.keys(p.trackedStocks).map((el) => (
             <option key={el + "ddl"} value={el}>
                 {p.trackedStocks[el].dStock(p.exchangeList)}
             </option>
         ));
 
-        if (stockData !== undefined) {
-            let symbolSelectorDropDown = (
-                <>
-                    <div id='earningsCalendarBody' className="div-inline">
-                        {"  Stock:  "}
-                        <select data-testid="ECstockSelector" className="btn" value={targetStock} onChange={changeStockSelection}>
-                            {newStockList}
-                        </select>
 
-                        {"  Display:  "}
-                        <select data-testid="ECDisplaySelector" className="btn" value={display} onChange={changeValueSelection}>
-                            {newSymbolList}
-                        </select>
+        let symbolSelectorDropDown = (
+            <>
+                <div id='earningsCalendarBody'>
+                    <select data-testid="ECstockSelector" className="btn" value={targetStock} onChange={changeStockSelection}>
+                        {newStockList}
+                    </select>
+                    <button onClick={() => changePagination(-1)}>
+                        <i className="fa fa-backward" aria-hidden="true"></i>
+                    </button>
+                    <button onClick={() => changePagination(1)}>
+                        <i className="fa fa-forward" aria-hidden="true"></i>
+                    </button>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Heading</td>
+                            <td>Value</td>
+                        </tr>
+                    </thead>
+                    <tbody>{stockTable()}</tbody>
+                </table>
+            </>
+        );
+        return symbolSelectorDropDown;
 
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Quarter</td>
-                                <td>{display === 'EPS' ? 'EPS Estimate' : 'Revenue Estimate'}</td>
-                                <td>{display === 'EPS' ? 'EPS Actual' : 'Revenue Actual'}</td>
-                                <td>Variance</td>
-                                <td>Variance%</td>
-                            </tr>
-                        </thead>
-                        <tbody>{stockTable()}</tbody>
-                    </table>
-                </>
-            );
-            return symbolSelectorDropDown;
-        }
     }
 
     return (
@@ -375,3 +270,6 @@ export const EarningsCalendarFilters: filters = { //IF widget uses filters remem
     description: 'Date numbers are millisecond offset from now. Used for Unix timestamp calculations.',
 }
 
+fetch('https://finnhub.io/api/v1/calendar/earnings?from=2020-06-26&to=2022-06-24&symbol=AAPL&token=sandbox_bujhtnn48v6rigi03lsg')
+    .then((dat) => dat.json())
+    .then(((data) => { console.log(data) }))
