6db117cc69d8a3213062cb760844df7a7a8aa616
Glenn Slear <glennstreetman@gmail.com>
2022-01-20 17:44:48 -0800

    fix tests

--- src/widgets/Estimates/PriceTarget/priceTarget.mock.ts
+++ src/widgets/Estimates/PriceTarget/priceTarget.mock.ts
@@ -1,126 +1,117 @@
-import { rest } from 'msw'
+import { rest } from "msw";
 
-const testDashboard = { //setup containing a single dashboard, TEST, and one widget, 16218726766030.
+const testDashboard = {
+    //setup containing a single dashboard, TEST, and one widget, 16218726766030.
     savedDashBoards: {
         TEST: {
-            dashboardname: 'TEST',
-            globalstocklist: '{ "US-WMT": { "currency": "USD", "description": "WALMART INC", "displaySymbol": "WMT", "figi": "BBG000BWXBC2", "mic": "XNYS", "symbol": "WMT", "type": "Common Stock", "exchange": "US", "key": "US-WMT" }, "US-COST": { "currency": "USD", "description": "COSTCO WHOLESALE CORP", "displaySymbol": "COST", "figi": "BBG000F6H8W8", "mic": "XNAS", "symbol": "COST", "type": "Common Stock", "exchange": "US", "key": "US-COST" } }',
+            dashboardname: "TEST",
+            globalstocklist:
+                '{ "US-WMT": { "currency": "USD", "description": "WALMART INC", "displaySymbol": "WMT", "figi": "BBG000BWXBC2", "mic": "XNYS", "symbol": "WMT", "type": "Common Stock", "exchange": "US", "key": "US-WMT" }, "US-COST": { "currency": "USD", "description": "COSTCO WHOLESALE CORP", "displaySymbol": "COST", "figi": "BBG000F6H8W8", "mic": "XNAS", "symbol": "COST", "type": "Common Stock", "exchange": "US", "key": "US-COST" } }',
             id: 1875,
             widgetlist: {},
-        }
+        },
     },
     menuSetup: {
         watchListMenu: {
             column: 0,
             columnOrder: 0,
-            widgetConfig: 'menuWidget',
-            widgetHeader: 'WatchList',
-            widgetID: 'watchListMenu',
-            widgetType: 'watchListMenu',
-            xAxis: '5',
-            yAxis: '75'
+            widgetConfig: "menuWidget",
+            widgetHeader: "WatchList",
+            widgetID: "watchListMenu",
+            widgetType: "watchListMenu",
+            xAxis: "5",
+            yAxis: "75",
         },
         dashBoardMenu: {
             column: 0,
             columnOrder: 1,
-            widgetConfig: 'menuWidget',
-            widgetHeader: 'Saved Dashboards',
-            widgetID: 'dashBoardMenu',
-            widgetType: 'dashBoardMenu',
-            xAxis: '46',
-            yAxis: '149'
-        }
+            widgetConfig: "menuWidget",
+            widgetHeader: "Saved Dashboards",
+            widgetID: "dashBoardMenu",
+            widgetType: "dashBoardMenu",
+            xAxis: "46",
+            yAxis: "149",
+        },
     },
-    default: 'TEST',
-    message: ''
-}
+    default: "TEST",
+    message: "",
+};
 
-export const getDashboard_success =     //auto login check rejected.
-    rest.get("/dashboard", (req, res, ctx) => {
-        const resObj = testDashboard
-        // console.log('RETURNING DASHBOARD DATA MOCK')
-        return res(
-            ctx.status(200),
-            ctx.json(resObj)
-        )
-    })
+export const getDashboard_success = rest.get("/dashboard", (req, res, ctx) => {
+    //auto login check rejected.
+    const resObj = testDashboard;
+    // console.log('RETURNING DASHBOARD DATA MOCK')
+    return res(ctx.status(200), ctx.json(resObj));
+});
 
 //default data to be returned for test purposes for all stocks.
 const resData_WMT = {
-    "lastUpdated": "2019-06-03 00:00:00",
-    "symbol": "NFLX",
-    "targetHigh": 100,
-    "targetLow": 101,
-    "targetMean": 102,
-    "targetMedian": 103
-}
+    lastUpdated: "2019-06-03 00:00:00",
+    symbol: "NFLX",
+    targetHigh: 100,
+    targetLow: 101,
+    targetMean: 102,
+    targetMedian: 103,
+};
 
 const resData_COST = {
-    "lastUpdated": "2019-06-03 00:00:00",
-    "symbol": "NFLX",
-    "targetHigh": 200,
-    "targetLow": 201,
-    "targetMean": 202,
-    "targetMedian": 203
-}
+    lastUpdated: "2019-06-03 00:00:00",
+    symbol: "NFLX",
+    targetHigh: 200,
+    targetLow: 201,
+    targetMean: 202,
+    targetMedian: 203,
+};
 
-export const mockFinnHubData = //MOCK API REQUEST FOR THIS WIDGET. Remember to update api string on next line.
-    rest.get("https://finnhub.io/api/v1/stock/price-target*", (req, res, ctx) => {
-        const symbol = req.url.searchParams.get('symbol')
-        let resData = symbol === 'WMT' ? resData_WMT : resData_COST
-        return res(
-            ctx.status(200),
-            ctx.json(resData)
-        )
-    })
+export const mockFinnHubData = rest.get("https://finnhub.io/api/v1/stock/price-target*", (req, res, ctx) => {
+    //MOCK API REQUEST FOR THIS WIDGET. Remember to update api string on next line.
+    const symbol = req.url.searchParams.get("symbol");
+    let resData = symbol === "WMT" ? resData_WMT : resData_COST;
+    return res(ctx.status(200), ctx.json(resData));
+});
 
-export const getCheckLogin_success =     //auto login check rejected.
-    rest.get("/checkLogin", (req, res, ctx) => {
-        // console.log('check login')
-        return res(
-            ctx.status(200),
-            ctx.json({
-                apiKey: 'sandboxTestApiKey', //always include sandbox so that socket server doesnt setup.
-                login: 1,
-                ratelimit: 25,
-                apiAlias: 'alias',
-                widgetsetup: '{"EstimatesPriceTarget":true,"PriceSplits":true}',
-                exchangelist: ['US'],
-                defaultexchange: ['US',]
-            })
-        )
-    })
+export const getCheckLogin_success = rest.get("/checkLogin", (req, res, ctx) => {
+    //auto login check rejected.
+    // console.log('check login')
+    return res(
+        ctx.status(200),
+        ctx.json({
+            apiKey: "sandboxTestApiKey", //always include sandbox so that socket server doesnt setup.
+            login: 1,
+            ratelimit: 25,
+            apiAlias: "alias",
+            widgetsetup: '{"EstimatesPriceTarget":true,"PriceSplits":true}',
+            exchangelist: "US,AS",
+            defaultexchange: "US",
+        })
+    );
+});
 
 const exchangeData = [
     {
-        "currency": "USD",
-        "description": "TESLA INC",
-        "displaySymbol": "TSLA",
-        "figi": "BBG000N9MNX3",
-        "mic": "XNAS",
-        "symbol": "TSLA",
-        "type": "Common Stock",
-        "exchange": "US",
-        "key": "US-TSLA"
+        currency: "USD",
+        description: "TESLA INC",
+        displaySymbol: "TSLA",
+        figi: "BBG000N9MNX3",
+        mic: "XNAS",
+        symbol: "TSLA",
+        type: "Common Stock",
+        exchange: "US",
+        key: "US-TSLA",
     },
     {
-        "currency": "USD",
-        "description": "APPLE INC",
-        "displaySymbol": "AAPL",
-        "figi": "BBG000B9XRY4",
-        "mic": "XNAS",
-        "symbol": "AAPL",
-        "type": "Common Stock",
-        "exchange": "US",
-        "key": "US-AAPL"
-    }
-]
+        currency: "USD",
+        description: "APPLE INC",
+        displaySymbol: "AAPL",
+        figi: "BBG000B9XRY4",
+        mic: "XNAS",
+        symbol: "AAPL",
+        type: "Common Stock",
+        exchange: "US",
+        key: "US-AAPL",
+    },
+];
 
-
-export const mockExchangeData =
-    rest.get("https://finnhub.io/api/v1/stock/symbol", (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json(exchangeData)
-        )
-    })
+export const mockExchangeData = rest.get("https://finnhub.io/api/v1/stock/symbol", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(exchangeData));
+});
--- src/widgets/Estimates/PriceTarget/priceTargetBody.tsx
+++ src/widgets/Estimates/PriceTarget/priceTargetBody.tsx
@@ -1,38 +1,38 @@
-import * as React from "react"
+import * as React from "react";
 import { useState, forwardRef, useRef, useMemo } from "react";
-import { widget } from 'src/App'
+import { widget } from "src/App";
 import { finnHubQueue } from "src/appFunctions/appImport/throttleQueueAPI";
 
-import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { useAppDispatch, useAppSelector } from "../../../hooks";
 
-import { convertCamelToProper } from '../../../appFunctions/stringFunctions'
+import { convertCamelToProper } from "../../../appFunctions/stringFunctions";
 
 //hooks
-import { useDragCopy } from './../../widgetHooks/useDragCopy'
-import { useSearchMongoDb } from '../../widgetHooks/useSearchMongoDB'
-import { useBuildVisableData } from '../../widgetHooks/useBuildVisableData'
-import { useUpdateFocus } from './../../widgetHooks/useUpdateFocus'
+import { useDragCopy } from "./../../widgetHooks/useDragCopy";
+import { useSearchMongoDb } from "../../widgetHooks/useSearchMongoDB";
+import { useBuildVisableData } from "../../widgetHooks/useBuildVisableData";
+import { useUpdateFocus } from "./../../widgetHooks/useUpdateFocus";
 
 //widget components
 import StockSearchPane, { searchPaneProps } from "../../../components/stockSearchPane";
-import WidgetFocus from '../../../components/widgetFocus'
-import WidgetRemoveSecurityTable from '../../../components/widgetRemoveSecurityTable'
+import WidgetFocus from "../../../components/widgetFocus";
+import WidgetRemoveSecurityTable from "../../../components/widgetRemoveSecurityTable";
 // import { dStock } from './../../../appFunctions/formatStockSymbols'
 
-const useDispatch = useAppDispatch
-const useSelector = useAppSelector
+const useDispatch = useAppDispatch;
+const useSelector = useAppSelector;
 
 interface widgetProps {
-    config: any,
-    enableDrag: boolean,
-    filters: any,
-    finnHubQueue: finnHubQueue,
-    pagination: number,
-    showEditPane: number,
-    trackedStocks: any,
-    widgetCopy: widget,
-    widgetKey: string | number,
-    widgetType: string,
+    config: any;
+    enableDrag: boolean;
+    filters: any;
+    finnHubQueue: finnHubQueue;
+    pagination: number;
+    showEditPane: number;
+    trackedStocks: any;
+    widgetCopy: widget;
+    widgetKey: string | number;
+    widgetType: string;
 }
 
 function PriceTargetBody(p: widgetProps, ref: any) {
@@ -41,40 +41,53 @@ function PriceTargetBody(p: widgetProps, ref: any) {
 
     const startingWidgetCoptyRef = () => {
         if (isInitialMount.current === true) {
-            if (p.widgetCopy !== undefined && typeof p.widgetCopy.widgetID === 'number') {
-                return p.widgetCopy.widgetID
-            } else { return 0 }
-        } else { return 0 }
-    }
-
-    const [widgetCopy] = useState(startingWidgetCoptyRef())
-    const apiKey = useSelector((state) => { return state.apiKey })
-    const currentDashboard = useSelector((state) => { return state.currentDashboard })
-    const dashboardData = useSelector((state) => { return state.dashboardData })
-    const targetSecurity = useSelector((state) => { return state.targetSecurity })
-    const exchangeList = useSelector((state) => { return state.exchangeList.exchangeList })
-    const dashboardID = dashboardData?.[currentDashboard]?.['id'] ? dashboardData[currentDashboard]['id'] : -1
-
-    const rShowData = useSelector((state) => { //REDUX Data associated with this widget.
-        if (state.dataModel !== undefined &&
-            state.dataModel.created !== 'false' &&
-            state.showData.dataSet[p.widgetKey] !== undefined) {
-            const showData: object = state.showData.dataSet[p.widgetKey][p.config.targetSecurity]
-            return (showData)
+            if (p.widgetCopy !== undefined && typeof p.widgetCopy.widgetID === "number") {
+                return p.widgetCopy.widgetID;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
         }
-    })
+    };
 
-    const focusSecurityList = useMemo(() => { //remove if all securities should stay in focus.
-        return [p?.config?.targetSecurity]
-    }, [p?.config?.targetSecurity])
+    const [widgetCopy] = useState(startingWidgetCoptyRef());
+    const apiKey = useSelector((state) => {
+        return state.apiKey;
+    });
+    const currentDashboard = useSelector((state) => {
+        return state.currentDashboard;
+    });
+    const dashboardData = useSelector((state) => {
+        return state.dashboardData;
+    });
+    const targetSecurity = useSelector((state) => {
+        return state.targetSecurity;
+    });
+    const exchangeList = useSelector((state) => {
+        return state.exchangeList.exchangeList;
+    });
+    const dashboardID = dashboardData?.[currentDashboard]?.["id"] ? dashboardData[currentDashboard]["id"] : -1;
 
-    useDragCopy(ref, {})//useImperativeHandle. Saves state on drag. Dragging widget pops widget out of component array causing 
-    useUpdateFocus(targetSecurity, p.widgetKey, p.config, dashboardData, currentDashboard, p.enableDrag, dispatch) //sets security focus in config. Used for redux.visable data and widget excel templating.
-    useSearchMongoDb(currentDashboard, p.finnHubQueue, p.config.targetSecurity, p.widgetKey, widgetCopy, dispatch, isInitialMount, dashboardID) //on change to target security retrieve fresh data from mongoDB
-    useBuildVisableData(focusSecurityList, p.widgetKey, widgetCopy, dispatch, isInitialMount) //rebuild visable data on update to target security
+    const rShowData = useSelector((state) => {
+        //REDUX Data associated with this widget.
+        if (state.dataModel !== undefined && state.dataModel.created !== "false" && state.showData.dataSet[p.widgetKey] !== undefined) {
+            const showData: object = state.showData.dataSet[p.widgetKey][p.config.targetSecurity];
+            return showData;
+        }
+    });
+
+    const focusSecurityList = useMemo(() => {
+        //remove if all securities should stay in focus.
+        return [p?.config?.targetSecurity];
+    }, [p?.config?.targetSecurity]);
+
+    useDragCopy(ref, {}); //useImperativeHandle. Saves state on drag. Dragging widget pops widget out of component array causing
+    useUpdateFocus(targetSecurity, p.widgetKey, p.config, dashboardData, currentDashboard, p.enableDrag, dispatch); //sets security focus in config. Used for redux.visable data and widget excel templating.
+    useSearchMongoDb(currentDashboard, p.finnHubQueue, p.config.targetSecurity, p.widgetKey, widgetCopy, dispatch, isInitialMount, dashboardID); //on change to target security retrieve fresh data from mongoDB
+    useBuildVisableData(focusSecurityList, p.widgetKey, widgetCopy, dispatch, isInitialMount); //rebuild visable data on update to target security
 
     function renderSearchPane() {
-
         let stockTable = (
             <WidgetRemoveSecurityTable
                 trackedStocks={p.trackedStocks}
@@ -85,63 +98,67 @@ function PriceTargetBody(p: widgetProps, ref: any) {
                 apiKey={apiKey}
             />
         );
-        return stockTable
+        return stockTable;
     }
 
     function renderStockData() {
-        const parseList = ['targetHigh', 'targetLow', 'targetMean', 'targetMedian']
+        const parseList = ["targetHigh", "targetLow", "targetMean", "targetMedian"];
 
-        const stockDataRows = rShowData ? Object.keys(rShowData).map((el) =>
-            <tr key={el + "row"}>
-                <td className='rightTE' key={el + "symbol"}>{convertCamelToProper(el)}: &nbsp;&nbsp;</td>
-                <td key={el + "name"}>{parseList.includes(el) ? parseInt(rShowData[el]).toLocaleString('en-US', { minimumFractionDigits: 2 }) : rShowData[el]}</td>
-            </tr>
-        ) : <></>
-        return <>
-            <WidgetFocus
-                widgetType={p.widgetType}
-                widgetKey={p.widgetKey}
-                trackedStocks={p.trackedStocks}
-                exchangeList={exchangeList}
-                config={p.config}
-                dashBoardData={dashboardData}
-                currentDashboard={currentDashboard}
-                enableDrag={p.enableDrag}
-            />
-            <div className='scrollableDiv'>
-                <table className='dataTable'>
-                    <thead>
-                        <tr>
-                            <td>Heading</td>
-                            <td>Value</td>
-                        </tr>
-                    </thead>
-                    <tbody data-testid='ptRow'>
-                        {stockDataRows}
-                    </tbody>
-                </table>
-            </div>
-        </>
+        const stockDataRows = rShowData ? (
+            Object.keys(rShowData).map((el) => (
+                <tr key={el + "row"}>
+                    <td className="rightTE" key={el + "symbol"}>
+                        {convertCamelToProper(el)}: &nbsp;&nbsp;
+                    </td>
+                    <td key={el + "name"}>
+                        {parseList.includes(el) ? parseInt(rShowData[el]).toLocaleString("en-US", { minimumFractionDigits: 2 }) : rShowData[el]}
+                    </td>
+                </tr>
+            ))
+        ) : (
+            <></>
+        );
+        return (
+            <>
+                <WidgetFocus
+                    widgetType={p.widgetType}
+                    widgetKey={p.widgetKey}
+                    trackedStocks={p.trackedStocks}
+                    exchangeList={exchangeList}
+                    config={p.config}
+                    dashBoardData={dashboardData}
+                    currentDashboard={currentDashboard}
+                    enableDrag={p.enableDrag}
+                />
+                <div className="scrollableDiv">
+                    <table className="dataTable">
+                        <thead>
+                            <tr>
+                                <td>Heading</td>
+                                <td>Value</td>
+                            </tr>
+                        </thead>
+                        <tbody data-testid="ptRow">{stockDataRows}</tbody>
+                    </table>
+                </div>
+            </>
+        );
     }
 
     return (
-        <div data-testid='priceTargetBody' >
+        <div data-testid="container-priceTargetBody">
             {p.showEditPane === 1 && (
                 <>
                     {React.createElement(StockSearchPane, searchPaneProps(p))}
                     {renderSearchPane()}
                 </>
             )}
-            {p.showEditPane === 0 && (
-                <>
-                    {renderStockData()}
-                </>
-            )}
+            {p.showEditPane === 0 && <>{renderStockData()}</>}
         </div>
-    )
+    );
 }
 
-export default forwardRef(PriceTargetBody)
+export default forwardRef(PriceTargetBody);
 
 export function priceTargetProps(that, key = "newWidgetNameProps") {
     let propList = {
@@ -157,5 +174,3 @@ export function priceTargetProps(that, key = "newWidgetNameProps") {
     };
     return propList;
 }
-
-
