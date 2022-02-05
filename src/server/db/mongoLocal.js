1ab16ad0443dd7226f1c44e8e1f9154a1a67a031
Glenn Slear <glennstreetman@gmail.com>
2022-02-05 10:10:50 -0800

    corrected error loggin messages and removed old comments

--- src/server/db/mongoLocal.js
+++ src/server/db/mongoLocal.js
@@ -1,27 +1,24 @@
-import MongoClient from 'mongodb'
-import dotenv from 'dotenv';
-dotenv.config()
+import MongoClient from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const uri = process.env.mongo;
-// const uri = 'mongodb://localhost:27017';
-// Create a new MongoClient
 let _db = new MongoClient.MongoClient(uri);
 
 const connectMongo = async (callback) => {
     try {
         MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-            _db = db
-            console.log("Connected2 to MongoDB")
-            return callback(err)
-        })
+            _db = db;
+            return callback(err);
+        });
     } catch (e) {
-        console.log("Failed to connect to MongoDB")
-        throw e
+        console.log("1-------Failed to connect to MongoDB-------------", e);
+        throw e;
     }
-}
+};
 
-const getDB = () => _db
+const getDB = () => _db;
 
-const disconnectDB = () => _db.close()
+const disconnectDB = () => _db.close();
 
-export {connectMongo, getDB, disconnectDB}
+export { connectMongo, getDB, disconnectDB };
