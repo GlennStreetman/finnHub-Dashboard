22214b1a6261a91dbe428ea84b8696f4bf3a474e
Glenn Slear <glennstreetman@gmail.com>
2022-02-05 10:12:55 -0800

    updated session types

--- src/server/routes/mongoDB/finnHubData.ts
+++ src/server/routes/mongoDB/finnHubData.ts
@@ -1,67 +1,64 @@
-import express from 'express';
-import { getDB } from '../../db/mongoLocal.js'
+import express from "express";
+import { getDB } from "../../db/mongoLocal.js";
 // import { finnHubData } from '../../db/mongoTypes'
 
 interface session {
-    login: boolean,
-    uID: number,
+    login: boolean;
+    uID: number;
 }
 
-
-
 interface finnDashDataReq extends Request {
-    session: session,
-    body: any
-    query: any
+    session: session;
+    body: any;
+    query: any;
 }
 
 const router = express.Router();
 
 //gets user, none stale, finnhub data. This process deletes stale records.
-router.get('/getFinnDashDataMongo', async (req: finnDashDataReq, res: any) => {
+router.get("/getFinnDashDataMongo", async (req: finnDashDataReq, res: any) => {
     try {
-        const client = getDB()
-        const database = client.db('finnDash');
-        const dataSet = database.collection('dataSet');
-        await dataSet.deleteMany({ //delete stale records for user
+        const client = getDB();
+        const database = client.db("finnDash");
+        const dataSet = database.collection("dataSet");
+        await dataSet.deleteMany({
+            //delete stale records for user
             userID: req.session.uID,
-            stale: { $lte: Date.now() } //delete records where stale date is saved as less than or equal to now.
-        })
+            stale: { $lte: Date.now() }, //delete records where stale date is saved as less than or equal to now.
+        });
 
         const reqFilter = {
             userID: req.session.uID,
-        }
-        if (req.query['dashboard']) reqFilter['dashboard'] = req.query['dashboard']
-        if (req.query['widget']) reqFilter['widget'] = req.query['widget']
-        const findDataSet = dataSet.find(reqFilter)
-        const resList: any[] = []
+        };
+        if (req.query["dashboard"]) reqFilter["dashboard"] = req.query["dashboard"];
+        if (req.query["widget"]) reqFilter["widget"] = req.query["widget"];
+        const findDataSet = dataSet.find(reqFilter);
+        const resList: any[] = [];
         await findDataSet.forEach((data: any) => {
-            resList.push(data)
-        })
-        res.status(200).json({ resList })
-    }
-    catch (err) {
+            resList.push(data);
+        });
+        res.status(200).json({ resList });
+    } catch (err) {
         res.status(500).json({ message: `Problem finding user dataset.` });
     }
-})
+});
 
-
-
-router.post("/postFinnDashDataMongo", async (req: finnDashDataReq, res: any) => {//updates MongoDB finnDash.dataset with finnhub data.
+router.post("/postFinnDashDataMongo", async (req: finnDashDataReq, res: any) => {
+    //updates MongoDB finnDash.dataset with finnhub data.
     if (req.session.login === true) {
         try {
-            const client = getDB()
-            const database = client.db('finnDash');
-            const dataSet = database.collection('dataSet');
+            const client = getDB();
+            const database = client.db("finnDash");
+            const dataSet = database.collection("dataSet");
 
-            const updateData = req.body
+            const updateData = req.body;
             for (const record in updateData) {
-                const u = updateData[record]
+                const u = updateData[record];
 
                 const filters = {
                     userID: req.session.uID,
                     key: record,
-                }
+                };
                 const update = {
                     $set: {
                         userID: req.session.uID,
@@ -76,23 +73,22 @@ router.post("/postFinnDashDataMongo", async (req: finnDashDataReq, res: any) => 
                         apiString: u.apiString,
                         security: u.security,
                         widgetType: u.widgetType,
-                        config: u.config
-                    }
-                }
+                        config: u.config,
+                    },
+                };
                 const options = {
-                    upsert: true
-                }
-                await dataSet.updateOne(filters, update, options)
-                    .catch((err) => { console.log('Problem updating dataset', err) })
+                    upsert: true,
+                };
+                await dataSet.updateOne(filters, update, options).catch((err) => {
+                    console.log("Problem updating dataset", err);
+                });
             }
             res.status(200).json({ message: `Updates Complete` });
-        }
-        catch (err) {
-            console.log('Problem updating finnHub dataset:', err)
-            res.status(500).json({ message: `Problem updating finnHub dataset` })
+        } catch (err) {
+            console.log("finnHubData: Problem updating finnHub dataset:", err);
+            res.status(500).json({ message: `Problem updating finnHub dataset` });
         }
     }
-})
+});
 
-export default router
-
+export default router;
