ec3d7ce0b1c77f2c1047ad11bca89c28716e10fb
Glenn Slear <glennstreetman@gmail.com>
2022-02-06 13:44:54 -0800

    added temp password check

--- src/appFunctions/client/checkPassword.js
+++ src/appFunctions/client/checkPassword.js
@@ -8,3 +8,14 @@ export const checkPassword = async function (email, pwText) {
     data.status = res.status;
     return data;
 };
+
+export const checkTempPassword = async function (pwText) {
+    let res = await fetch(`/tempLogin?pwText=${pwText}`);
+    if (res.status === 500) {
+        console.log("No Response from server");
+        throw new Error("Problem with server");
+    }
+    let data = await res.json();
+    data.status = res.status;
+    return data;
+};
