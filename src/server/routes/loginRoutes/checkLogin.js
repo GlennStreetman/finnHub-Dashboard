6accb7347078b796f20464299d0752ef8f73ed8f
glenn <glennstreetman@gmail.com>
2022-02-25 05:34:54 +0000

    stash progress on remote login addon to checkLogin

--- src/server/routes/loginRoutes/checkLogin.js
+++ src/server/routes/loginRoutes/checkLogin.js
@@ -1,11 +1,12 @@
 import express from "express";
 import postgresDB from "../../db/databaseLocalPG.js";
+import axios from 'axios'
 
 const router = express.Router();
 
-//checks login status when site is initialy loaded.
-router.get("/checkLogin", (req, res, next) => {
+const retrieveAPIKeys = (req, next) => {
     try {
+    return new Promise((resolve, reject) => {
         if (req.session === undefined) throw new Error("Request not associated with session.");
         const db = postgresDB;
         const resData = { login: 0 }; //
@@ -14,47 +15,46 @@ router.get("/checkLogin", (req, res, next) => {
         SELECT apikey, apialias, ratelimit, exchangelist, defaultexchange, widgetsetup, templogin
         FROM users
         WHERE id = ${uID}
-    `; //add a timeout to the WHERE clause on resetpasswordlink using templogin timestamp
-        const retrieveAPIKeys = () => {
-            console.log("checking login status", apiKeysQuery);
-            return new Promise((resolve, reject) => {
-                db.query(apiKeysQuery, (err, rows) => {
-                    if (err) {
-                        console.log("error retrieving apiKeys");
-                        reject(resData);
-                    } else {
-                        console.log("getting user data");
-                        resData.apiKey = rows.rows[0].apikey;
-                        resData.apiAlias = rows.rows[0].apialias;
-                        resData.exchangelist = rows.rows[0].exchangelist;
-                        resData.defaultexchange = rows.rows[0].defaultexchange;
-                        resData.login = 1;
-                        resData.ratelimit = rows.rows[0].ratelimit;
-                        resData.widgetsetup = rows.rows[0].widgetsetup;
-                        console.log("returning user data", resData);
-                        resolve(resData);
-                    }
-                });
-            });
-        };
-
-        retrieveAPIKeys()
-            .then((data) => {
-                if (req.session.login === true) {
-                    console.log("session login is true");
-                    res.status(200).json(data);
-                } else {
-                    console.log("not logged in");
-                    res.status(401).json({ login: 0 });
-                }
-            })
-            .catch((err) => {
-                console.log("/checkLogin", err);
-                res.status(401).json(err);
-            });
+        `;
+        
+        db.query(apiKeysQuery, (err, rows) => {
+            if (err) {
+                console.log("error retrieving apiKeys");
+                reject(resData);
+            } else {
+                console.log("getting user data");
+                resData.apiKey = rows.rows[0].apikey;
+                resData.apiAlias = rows.rows[0].apialias;
+                resData.exchangelist = rows.rows[0].exchangelist;
+                resData.defaultexchange = rows.rows[0].defaultexchange;
+                resData.login = 1;
+                resData.ratelimit = rows.rows[0].ratelimit;
+                resData.widgetsetup = rows.rows[0].widgetsetup;
+                console.log("returning user data", resData);
+                resolve(resData);
+            }
+        });
+    });
     } catch (error) {
         next(error);
     }
+};
+
+//checks login status when site is initialy loaded.
+router.get("/checkLogin", async (req, res, next) => {
+
+    if (req.session && req.session.login === true) {
+        console.log('getting local login') 
+        const apiKeys = await retrieveAPIKeys(req, next)
+        res.status(200).json(apiKeys);
+    } else {
+        console.log('checking remote login')
+        const axiosRes = await axios('http://gstreet.test/api/remoteLogin', {method: 'GET', mode: '*'}).catch((err)=>{console.log('axios err', err)})
+        console.log('res', axiosRes.data)
+        console.log("not logged in");
+        res.status(401).json({ login: 0 });
+    }
+    
 });
 
 export default router;
