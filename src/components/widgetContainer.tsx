4bf13f972cfa49a7005c91b7d8fcd20e901058c0
Glenn Streetman <glennstreetman@gmail.com>
2022-08-24 02:15:59 +0000

    add styled icon buttons

--- src/components/widgetContainer.tsx
+++ src/components/widgetContainer.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import IconButton from "@mui/material/IconButton";
+import { FaList } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { ImFileExcel } from "react-icons/im";
+import {
+    AiOutlineEdit,
+    AiFillDownCircle,
+    AiFillRightCircle,
+    AiOutlineDrag,
+} from "react-icons/ai";
+
 import ToolTip from "./toolTip";
 import ErrorBoundary from "./widgetErrorBoundary";
 import { useAppDispatch, useAppSelector } from "src/hooks";
@@ -267,53 +278,60 @@ function WidgetContainer(p: containerProps) {
     }
 
     return (
-        <div
-            key={p.widgetKey + "container" + p.widgetList.column}
-            id={p.widgetKey + "box"}
-            style={compStyle}
-            className="widgetBox"
-            data-testid={`container-${p.widgetList.widgetType}`}
-        >
+        <IconContext.Provider value={{ color: "white", size: ".75em" }}>
             <div
-                data-testid={
-                    p.stateRef === "menuWidget" ? "menuHeader" : "widgetHeader"
-                }
-                className={
-                    p.stateRef === "menuWidget" ? "menuHeader" : "widgetHeader"
-                }
+                key={p.widgetKey + "container" + p.widgetList.column}
+                id={p.widgetKey + "box"}
+                style={compStyle}
+                className="widgetBox"
+                data-testid={`container-${p.widgetList.widgetType}`}
             >
-                {showEditPane === 0 ? (
-                    <>
-                        {widgetProps["helpText"] !== undefined && (
-                            <ToolTip
-                                textFragment={widgetProps["helpText"][0]}
-                                hintName={widgetProps["helpText"][1]}
-                            />
-                        )}
-                        {renderHeader}
-                    </>
-                ) : (
-                    <input
-                        data-testid={`rename-${p.widgetList["widgetType"]}`}
-                        type="text"
-                        id={p.widgetKey + "HeaderValue"}
-                        value={renderHeader}
-                        onChange={trackUpdate}
-                        onBlur={updateHeader}
-                    />
-                )}
-
-                <button
-                    className="widgetButtonHead"
-                    id={`${p.widgetList["widgetID"]}`}
-                    onMouseOver={() => {
-                        dragElement();
-                    }}
+                <div
+                    data-testid={
+                        p.stateRef === "menuWidget"
+                            ? "menuHeader"
+                            : "widgetHeader"
+                    }
+                    className={
+                        p.stateRef === "menuWidget"
+                            ? "menuHeader"
+                            : "widgetHeader"
+                    }
                 >
-                    <i className="fa fa-arrows" aria-hidden="true"></i>
-                </button>
-                <>
-                    <button
+                    {showEditPane === 0 ? (
+                        <>
+                            {widgetProps["helpText"] !== undefined && (
+                                <ToolTip
+                                    textFragment={widgetProps["helpText"][0]}
+                                    hintName={widgetProps["helpText"][1]}
+                                />
+                            )}
+                            {renderHeader}
+                        </>
+                    ) : (
+                        <input
+                            data-testid={`rename-${p.widgetList["widgetType"]}`}
+                            type="text"
+                            id={p.widgetKey + "HeaderValue"}
+                            value={renderHeader}
+                            onChange={trackUpdate}
+                            onBlur={updateHeader}
+                        />
+                    )}
+
+                    <IconButton
+                        sx={{ padding: "2px" }}
+                        className="widgetButtonHead"
+                        id={`${p.widgetList["widgetID"]}`}
+                        onMouseOver={() => {
+                            dragElement();
+                        }}
+                    >
+                        <AiOutlineDrag aria-hidden="true" />
+                    </IconButton>
+
+                    <IconButton
+                        sx={{ padding: "2px" }}
                         className="widgetButtonHead"
                         onClick={() => {
                             const toggled = toggleWidgetBody(
@@ -336,131 +354,130 @@ function WidgetContainer(p: containerProps) {
                         }}
                     >
                         {p.widgetList.showBody !== false ? (
-                            <i
-                                className="fa fa-caret-square-o-down"
-                                aria-hidden="true"
-                            />
+                            <AiFillDownCircle className="" aria-hidden="true" />
                         ) : (
-                            <i
-                                className="fa fa-caret-square-o-right"
+                            <AiFillRightCircle
+                                className=""
                                 aria-hidden="true"
                             />
                         )}
-                    </button>
+                    </IconButton>
 
-                    <button
+                    <IconButton
+                        sx={{ padding: "2px" }}
                         data-testid={`editPaneButton-${p.widgetList["widgetType"]}`}
                         className="widgetButtonHead"
                         onClick={() => {
                             showPane(setShowEditPane, -1);
                         }}
                     >
-                        <i
-                            className="fa fa-pencil-square-o"
-                            aria-hidden="true"
-                        />
-                    </button>
-                </>
-            </div>
-            {p.widgetList.showBody !== false ? (
-                <div className="widgetBody">
-                    {/* @ts-ignore */}
-                    <ErrorBoundary widgetType={p.widgetList["widgetType"]}>
-                        {React.createElement(
-                            widgetLookUp[p.widgetList["widgetType"]],
-                            { ref: widgetRef, ...widgetProps }
-                        )}
-                    </ErrorBoundary>
+                        <AiOutlineEdit className="" aria-hidden="true" />
+                    </IconButton>
                 </div>
-            ) : (
-                <></>
-            )}
+                {p.widgetList.showBody !== false ? (
+                    <div className="widgetBody">
+                        {/* @ts-ignore */}
+                        <ErrorBoundary widgetType={p.widgetList["widgetType"]}>
+                            {React.createElement(
+                                widgetLookUp[p.widgetList["widgetType"]],
+                                { ref: widgetRef, ...widgetProps }
+                            )}
+                        </ErrorBoundary>
+                    </div>
+                ) : (
+                    <></>
+                )}
 
-            <div className="widgetFooter">
-                {p.widgetList.showBody !== false &&
-                    showEditPane === 1 &&
-                    (p.stateRef !== "menuWidget" ? (
-                        <button
-                            onClick={async () => {
-                                if (
-                                    p.stateRef === "stockWidget" ||
-                                    p.stateRef === "marketWidget"
-                                ) {
-                                    const payload: removeWidgetPayload = {
-                                        widgetKey: p.widgetKey,
-                                        dashboardName: currentDashboard,
-                                    };
-                                    dispatch(rRemoveWidget(payload));
-                                    dispatch(rUnmountWidget(payload));
-                                    dispatch(rRemoveWidgetDataModel(payload));
-                                    dispatch(
-                                        tSaveDashboard({
+                <div className="widgetFooter">
+                    {p.widgetList.showBody !== false &&
+                        showEditPane === 1 &&
+                        (p.stateRef !== "menuWidget" ? (
+                            <button
+                                onClick={async () => {
+                                    if (
+                                        p.stateRef === "stockWidget" ||
+                                        p.stateRef === "marketWidget"
+                                    ) {
+                                        const payload: removeWidgetPayload = {
+                                            widgetKey: p.widgetKey,
                                             dashboardName: currentDashboard,
-                                        })
+                                        };
+                                        dispatch(rRemoveWidget(payload));
+                                        dispatch(rUnmountWidget(payload));
+                                        dispatch(
+                                            rRemoveWidgetDataModel(payload)
+                                        );
+                                        dispatch(
+                                            tSaveDashboard({
+                                                dashboardName: currentDashboard,
+                                            })
+                                        );
+                                        fetch(
+                                            `/api/deleteFinnDashData?widgetID=${p.widgetKey}`
+                                        ); //delete from mongo.
+                                    }
+                                }}
+                            >
+                                <i
+                                    className="fa fa-times"
+                                    aria-hidden="true"
+                                    data-testid={`removeWidget-${p.widgetList["widgetType"]}`}
+                                ></i>
+                            </button>
+                        ) : (
+                            <></>
+                        ))}
+
+                    {p.widgetList.showBody !== false &&
+                        showEditPane !== 1 &&
+                        excelRegister_singleSecurity[
+                            p.widgetList.widgetType
+                        ] && ( //button returns data for target securities associated with widget.
+                            <IconButton
+                                className="widgetButtonFoot"
+                                onClick={() => {
+                                    excelFunction(
+                                        apiKey,
+                                        currentDashboard,
+                                        p.widgetList.widgetHeader,
+                                        p.widgetList.config.targetSecurity,
+                                        p.widgetList.config
                                     );
-                                    fetch(
-                                        `/api/deleteFinnDashData?widgetID=${p.widgetKey}`
-                                    ); //delete from mongo.
-                                }
-                            }}
-                        >
-                            <i
-                                className="fa fa-times"
-                                aria-hidden="true"
-                                data-testid={`removeWidget-${p.widgetList["widgetType"]}`}
-                            ></i>
-                        </button>
-                    ) : (
-                        <></>
-                    ))}
+                                }}
+                            >
+                                <ImFileExcel
+                                    className=""
+                                    aria-hidden="true"
+                                    data-testid={`excelButton-${p.widgetList["widgetType"]}`}
+                                ></ImFileExcel>
+                            </IconButton>
+                        )}
 
-                {p.widgetList.showBody !== false &&
-                    showEditPane !== 1 &&
-                    excelRegister_singleSecurity[p.widgetList.widgetType] && ( //button returns data for target securities associated with widget.
-                        <button
-                            className="widgetButtonFoot"
-                            onClick={() => {
-                                excelFunction(
-                                    apiKey,
-                                    currentDashboard,
-                                    p.widgetList.widgetHeader,
-                                    p.widgetList.config.targetSecurity,
-                                    p.widgetList.config
-                                );
-                            }}
-                        >
-                            <i
-                                className="fa fa-file-excel-o"
-                                aria-hidden="true"
-                                data-testid={`excelButton-${p.widgetList["widgetType"]}`}
-                            ></i>
-                        </button>
-                    )}
-
-                {p.widgetList.showBody !== false &&
-                    showEditPane !== 1 &&
-                    excelRegister[p.widgetList.widgetType] && ( //button returns data for all securities associated with widget.
-                        <button
-                            className="widgetButtonFoot"
-                            onClick={() => {
-                                excelFunction(
-                                    apiKey,
-                                    currentDashboard,
-                                    p.widgetList.widgetHeader,
-                                    false,
-                                    p.widgetList.config
-                                );
-                            }}
-                        >
-                            <i
-                                className="fa fa-list"
-                                aria-hidden="true"
-                                data-testid={`excelButton-${p.widgetList["widgetType"]}`}
-                            ></i>
-                        </button>
-                    )}
+                    {p.widgetList.showBody !== false &&
+                        showEditPane !== 1 &&
+                        excelRegister[p.widgetList.widgetType] && ( //button returns data for all securities associated with widget.
+                            <IconButton
+                                className="widgetButtonFoot"
+                                onClick={() => {
+                                    excelFunction(
+                                        apiKey,
+                                        currentDashboard,
+                                        p.widgetList.widgetHeader,
+                                        false,
+                                        p.widgetList.config
+                                    );
+                                }}
+                            >
+                                <FaList
+                                    className=""
+                                    aria-hidden="true"
+                                    data-testid={`excelButton-${p.widgetList["widgetType"]}`}
+                                />
+                            </IconButton>
+                        )}
+                </div>
             </div>
-        </div>
+        </IconContext.Provider>
     );
 }
 
