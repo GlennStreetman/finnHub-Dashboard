3eb3891bb6ed8704d4b37457c6a79c9c003d252c
Glenn Streetman <glennstreetman@gmail.com>
2022-08-26 21:11:10 +0000

    debut excel templating overrides

--- src/server/routes/excelTemplates/actions/buildTemplateData.tsx
+++ src/server/routes/excelTemplates/actions/buildTemplateData.tsx
@@ -1,64 +1,93 @@
-import Excel from 'exceljs';
-import { getDataSlice } from './getDataSlice.js'
+import Excel from "exceljs";
+import { getDataSlice } from "./getDataSlice.js";
 // import { GQLReqObj } from './getGraphQLData.js'
-import { processedPromiseData } from './processPromiseData.js'
+import { processedPromiseData } from "./processPromiseData.js";
 
 interface thisRow {
-    data: Object,
-    writeRows: { [key: string]: number }, //key is security id. number is row counts
-    keyColumns: Object
+    data: Object;
+    writeRows: { [key: string]: number }; //key is security id. number is row counts
+    keyColumns: Object;
 }
 
 interface worksheetData {
-    sheetKeys: Set<string>,
-    [key: number]: thisRow
+    sheetKeys: Set<string>;
+    [key: number]: thisRow;
 }
 
 export interface templateData {
-    [key: string]: worksheetData
+    [key: string]: worksheetData;
 }
 
-async function buildTemplateData(promiseData: processedPromiseData, workBookPath: string): Promise<templateData> {
+async function buildTemplateData(
+    promiseData: processedPromiseData,
+    workBookPath: string
+): Promise<templateData> {
     //transform promise data
-    const wb = new Excel.Workbook()
-    await wb.xlsx.readFile(workBookPath)
-    const templateData = {}
-    wb.eachSheet((worksheet) => { //for each worksheet not named Query.
-        if (worksheet.name !== 'Query') {
-            templateData[worksheet.name] = { sheetKeys: new Set() } //sheet keys used in case data points need to be split into multiple worksheets.
-            worksheet.eachRow((row, rowNumber) => { //for each row in sheet.
+    const wb = new Excel.Workbook();
+    await wb.xlsx.readFile(workBookPath);
+    const templateData = {};
+    wb.eachSheet((worksheet) => {
+        //for each worksheet not named Query.
+        if (worksheet.name !== "Query") {
+            templateData[worksheet.name] = { sheetKeys: new Set() }; //sheet keys used in case data points need to be split into multiple worksheets.
+            worksheet.eachRow((row, rowNumber) => {
+                //for each row in sheet.
                 const thisRow = {
                     data: {},
                     writeRows: {}, //if zero skip in later steps. Used for inserting new rows if greater than 1.
                     keyColumns: {}, //list of columns where key needs to be updated.
-                }
-                for (const x in row.values) { //for each value in row.
-                    if (typeof row?.values?.[x] === 'string' && row?.values?.[x]?.slice(0, 2) === '&=') { //if template formula detected.
-                        const searchStringRaw = row.values[x]
-                        const searchString = searchStringRaw.slice(2, searchStringRaw.length)
-                        if (searchString !== 'keys.keys') { //if data column
-                            const dataObj = getDataSlice(promiseData, searchString) //could {key: string} pairs OR {key: OBJECT} pairs
+                };
+                for (const x in row.values) {
+                    //for each value in row.
+                    if (
+                        typeof row?.values?.[x] === "string" &&
+                        row?.values?.[x]?.slice(0, 2) === "&="
+                    ) {
+                        //if template formula detected.
+                        const searchStringRaw = row.values[x];
+                        const searchString = searchStringRaw.slice(
+                            2,
+                            searchStringRaw.length
+                        );
+                        if (searchString !== "keys.keys") {
+                            //if data column
+                            const dataObj = getDataSlice(
+                                promiseData,
+                                searchString
+                            ); //could {key: string} pairs OR {key: OBJECT} pairs
                             for (const security in dataObj) {
-                                templateData[worksheet.name]['sheetKeys'].add(security)
-                                if (typeof dataObj[security] === 'object') { //count number of rows if time series data in dataset.
+                                templateData[worksheet.name]["sheetKeys"].add(
+                                    security
+                                );
+                                if (typeof dataObj[security] === "object") {
+                                    //count number of rows if time series data in dataset.
                                     // if (Object.keys(dataObj[security]).length > thisRow.writeRows) thisRow.writeRows = Object.keys(dataObj[s]).length
-                                    thisRow.writeRows[security] = Object.keys(dataObj[security]).length ? Object.keys(dataObj[security]).length : 1
+                                    thisRow.writeRows[security] = Object.keys(
+                                        dataObj[security]
+                                    ).length
+                                        ? Object.keys(dataObj[security]).length
+                                        : 1;
                                 }
                             }
-                            if (Object.keys(dataObj).length > thisRow.writeRows) { thisRow.writeRows = Object.keys(dataObj).length }
-                            thisRow.data[x] = dataObj
-                        } else { //if key column
-                            thisRow.data[x] = { ...promiseData.keys }
-                            thisRow.keyColumns[x] = x
+                            if (
+                                Object.keys(dataObj).length > thisRow.writeRows
+                            ) {
+                                thisRow.writeRows = Object.keys(dataObj).length;
+                            }
+                            thisRow.data[x] = dataObj;
+                        } else {
+                            //if key column
+                            thisRow.data[x] = { ...promiseData.keys };
+                            thisRow.keyColumns[x] = x;
                         }
                     }
                 }
-                templateData[worksheet.name][rowNumber] = thisRow
-            })
+                templateData[worksheet.name][rowNumber] = thisRow;
+            });
         }
-    })
-
-    return templateData //object describing data that needs to be populated with data.
+    });
+    // console.log("complete template data", templateData);
+    return templateData; //object describing data that needs to be populated with data.
 }
 
-export { buildTemplateData }
+export { buildTemplateData };
--- src/server/routes/excelTemplates/actions/copyCharts.ts
+++ src/server/routes/excelTemplates/actions/copyCharts.ts
@@ -1,103 +1,201 @@
-import fs from 'fs';
-import { makeTempDir } from './makeTempDir.js';
+import fs from "fs";
+import { makeTempDir } from "./makeTempDir.js";
 
-import copyExcelChart from 'copy-excel-chart'
-import { templateData } from './buildTemplateData.js'
+import copyExcelChart from "copy-excel-chart";
+import { templateData } from "./buildTemplateData.js";
 
+const readCharts = copyExcelChart.readCharts;
+const copyChart = copyExcelChart.copyChart;
+const writeCharts = copyExcelChart.writeCharts;
 
-
-const readCharts = copyExcelChart.readCharts
-const copyChart = copyExcelChart.copyChart
-const writeCharts = copyExcelChart.writeCharts
-
-function updateWorksheetRefsMulti(worksheetList: string[], excelCellRef: string, outputRef: string) {
-    let newCellRef = excelCellRef
+function updateWorksheetRefsMulti(
+    worksheetList: string[],
+    excelCellRef: string,
+    outputRef: string
+) {
+    let newCellRef = excelCellRef;
     worksheetList.forEach((ws) => {
-        newCellRef = newCellRef.replace(`${ws}!`, `${ws}-${outputRef}!`)
-    })
+        newCellRef = newCellRef.replace(`${ws}!`, `${ws}-${outputRef}!`);
+    });
 
-    return newCellRef
+    return newCellRef;
 }
 
-export default async function copyCharts(templateData: templateData, fromFile: string, toFile: string, xmlFolder: string, tempFolder: string, outputFileName: string, multiSheet: string) {
+function updateWorksheetRefsSingle(
+    worksheetList: string[],
+    excelCellRef: string,
+    outputRef: string
+) {
+    let newCellRef = excelCellRef;
+    worksheetList.forEach((ws) => {
+        newCellRef = newCellRef.replace(`${ws}!`, `${ws}-${outputRef}!`);
+    });
 
+    return newCellRef;
+}
+
+export default async function copyCharts(
+    templateData: templateData,
+    fromFile: string,
+    toFile: string,
+    xmlFolder: string,
+    tempFolder: string,
+    outputFileName: string,
+    multiSheet: string
+) {
     try {
+        if (!fs.existsSync(`${xmlFolder}`)) makeTempDir(`${xmlFolder}`); //location of xmls.
 
-        if (!fs.existsSync(`${xmlFolder}`)) makeTempDir(`${xmlFolder}`) //location of xmls.
+        const source = await readCharts(fromFile, `${xmlFolder}`);
+        const outputSource = await readCharts(toFile, `${xmlFolder}`);
 
-        const source = await readCharts(fromFile, `${xmlFolder}`)
-        const output = await readCharts(toFile, `${xmlFolder}`)
+        console.log("source summary");
 
-        if (multiSheet === 'true') {
+        if (multiSheet === "true") {
+            for (const sourceWorksheet of Object.keys(templateData)) {
+                //for each worksheet
+                //for each worksheet
+                const outputSheets: string[] | undefined = templateData?.[
+                    sourceWorksheet
+                ]?.sheetKeys
+                    ? [...templateData[sourceWorksheet].sheetKeys]
+                    : undefined;
 
-            for (const sourceWorksheet of Object.keys(templateData)) { //for each worksheet
-                const outputSheets: string[] | undefined = templateData?.[sourceWorksheet]?.sheetKeys ? [...templateData[sourceWorksheet].sheetKeys] : undefined
+                if (outputSheets)
+                    for (const outputWorksheetRef of outputSheets) {
+                        // for each associated output sheet.
+                        const outputWorksheetAlias = `${sourceWorksheet}-${outputWorksheetRef}`;
 
-                if (outputSheets) for (const outputWorksheetRef of outputSheets) { // for each associated output sheet.
-                    const outputWorksheetAlias = `${sourceWorksheet}-${outputWorksheetRef}`
+                        const worksheetSummaryItems = Object.entries(
+                            source.summary()[sourceWorksheet]
+                        );
+                        if (worksheetSummaryItems)
+                            for (const x of worksheetSummaryItems) {
+                                // for each chart in worksheet
 
-                    const worksheetSummaryItems = Object.entries(source.summary()[sourceWorksheet])
-                    if (worksheetSummaryItems) for (const x of worksheetSummaryItems) { // for each chart in worksheet
+                                const chart = x[0];
 
-                        const chart = x[0]
+                                const replaceCellRefs: {
+                                    [key: string]: string;
+                                } = source
+                                    .summary()
+                                    [sourceWorksheet][chart].reduce(
+                                        (acc, ref, indx) => {
+                                            let newRef =
+                                                updateWorksheetRefsMulti(
+                                                    Object.keys(templateData),
+                                                    ref,
+                                                    outputWorksheetRef
+                                                );
+                                            return { ...acc, [ref]: newRef };
+                                        },
+                                        {}
+                                    );
 
-                        const replaceCellRefs: { [key: string]: string } = source.summary()[sourceWorksheet][chart].reduce((acc, ref, indx) => {
-                            let newRef = updateWorksheetRefsMulti(Object.keys(templateData), ref, outputWorksheetRef)
-                            return { ...acc, [ref]: newRef }
-                        }, {})
+                                Object.entries(replaceCellRefs).forEach(
+                                    ([key, val]) => {
+                                        const keySheet = key.slice(
+                                            0,
+                                            key.indexOf("!")
+                                        );
+                                        // console.log(templateData[keySheet][2].writeRows, outputWorksheetRef)
+                                        const writeRows =
+                                            templateData[keySheet][2].writeRows[
+                                                outputWorksheetRef
+                                            ];
+                                        const newVal = !val.includes(":")
+                                            ? val
+                                            : `${val.slice(
+                                                  0,
+                                                  val.lastIndexOf("$")
+                                              )}${writeRows}`;
+                                        replaceCellRefs[key] = newVal;
+                                    }
+                                );
 
-                        Object.entries(replaceCellRefs).forEach(([key, val]) => {
-                            const keySheet = key.slice(0, key.indexOf('!'))
-                            // console.log(templateData[keySheet][2].writeRows, outputWorksheetRef)
-                            const writeRows = templateData[keySheet][2].writeRows[outputWorksheetRef]
-                            const newVal = !val.includes(':') ? val : `${val.slice(0, val.lastIndexOf('$'))}${writeRows}`
-                            replaceCellRefs[key] = newVal
+                                // console.log('replaceCellRefs', replaceCellRefs)
 
-                        })
-
-                        // console.log('replaceCellRefs', replaceCellRefs)
-
-                        await copyChart(
-                            source,
-                            output,
-                            sourceWorksheet,
-                            chart,
-                            outputWorksheetAlias,
-                            replaceCellRefs,
-                        )
+                                await copyChart(
+                                    source,
+                                    outputSource,
+                                    sourceWorksheet,
+                                    chart,
+                                    outputWorksheetAlias,
+                                    replaceCellRefs
+                                );
+                            }
                     }
-                }
             }
         } else {
-            // console.log('processing single')
-            for (const sourceWorksheet of Object.keys(templateData)) { //for each worksheet
+            for (const sourceWorksheet of Object.keys(templateData)) {
+                // for each worksheet
+                const worksheetSummaryItems = Object.entries(
+                    source.summary()[sourceWorksheet]
+                );
+                if (worksheetSummaryItems)
+                    for (const [chartKey, overrides] of worksheetSummaryItems) {
+                        // console.log("chartKey", chartKey, "overrides: ", overrides);
+                        // console.log("output summary", outputSource.summary());
+                        const replaceCellRefs: {
+                            [key: string]: string;
+                        } = source
+                            .summary()
+                            [sourceWorksheet][chartKey].reduce(
+                                (acc, ref, indx) => {
+                                    const thisIndex = (indx + 1).toString();
+                                    const baseSecurity = [
+                                        ...templateData[sourceWorksheet][
+                                            "sheetKeys"
+                                        ],
+                                    ][0];
+                                    const newEndRow = templateData?.[
+                                        sourceWorksheet
+                                    ]?.[thisIndex]?.["data"]?.[thisIndex]?.[
+                                        baseSecurity
+                                    ]
+                                        ? Object.keys(
+                                              templateData[sourceWorksheet][
+                                                  thisIndex
+                                              ]["data"][thisIndex][baseSecurity]
+                                          ).length
+                                        : undefined;
+                                    if (newEndRow) {
+                                        const endDollar =
+                                            ref.lastIndexOf("$") + 1;
+                                        const newRef =
+                                            ref.slice(0, endDollar) + newEndRow;
+                                        console.log("ref", newRef);
+                                        return { ...acc, [ref]: newRef };
+                                    } else {
+                                        return acc;
+                                    }
+                                },
+                                {}
+                            );
 
-                const worksheetSummaryItems = Object.entries(source.summary()[sourceWorksheet])
-                if (worksheetSummaryItems) for (const x of worksheetSummaryItems) { // for each chart in worksheet
-                    const chart = x[0]
-                    const replaceCellRefs = source.summary()[sourceWorksheet][chart].reduce((acc, ref) => {
-                        return { ...acc, [ref]: ref }
-                    }, {})
-                    // console.log('copy chart single -->', chart, replaceCellRefs)
-                    await copyChart(
-                        source,
-                        output,
-                        sourceWorksheet,
-                        chart,
-                        sourceWorksheet,
-                        replaceCellRefs,
-                    )
-                    // console.log('finish copy chart single', sourceWorksheet, chart)
-                }
+                        console.log("replaceCellRefs", replaceCellRefs);
+
+                        // console.log('copy chart single -->', chart, replaceCellRefs)
+                        await copyChart(
+                            source,
+                            outputSource,
+                            sourceWorksheet,
+                            chartKey,
+                            sourceWorksheet,
+                            replaceCellRefs
+                        );
+                        // console.log('finish copy chart single', sourceWorksheet, chart)
+                    }
             }
-
         }
 
-        const tempFileName = `${tempFolder}${outputFileName}${Date.now() / 1000}.xlsx`
-        await writeCharts(output, tempFileName)
+        const tempFileName = `${tempFolder}${outputFileName}${
+            Date.now() / 1000
+        }.xlsx`;
+        await writeCharts(outputSource, tempFileName);
 
-        return tempFileName
+        return tempFileName;
     } catch (error) {
-        console.log('CopyCharts.ts - Problem copying charts: ', error)
+        console.log("CopyCharts.ts - Problem copying charts: ", error);
     }
 }
